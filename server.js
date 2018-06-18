734aca82a23dba93081f6549a9ca74cfbac7c57c
Arsen Babajanyan <arsenbabajanyan95@gmail.com>
2018-06-18 16:27:31 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.js')
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
